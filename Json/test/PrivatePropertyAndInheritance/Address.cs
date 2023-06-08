5f83e4758cd6c6cad44e1fbf89cd5dbe6d2b31a0
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2023-06-08 18:16:46 -0300

    habilita sourcelink na lib json

---
+++ Json/test/PrivatePropertyAndInheritance/Address.cs
@@ -0,0 +1,9 @@
+﻿//using System.Text.Json.Serialization;
+
+//namespace Cblx.Blocks.Json.Tests.PrivatePropertyAndInheritance;
+
+//public class Address
+//{
+//    [JsonPropertyName("person_street")]
+//    public string Street { get; private set; } = "Elm Street";
+//}
---
+++ Json/test/PrivatePropertyAndInheritance/Person.cs
@@ -0,0 +1,23 @@
+﻿//using System.Text.Json.Serialization;
+
+//namespace Cblx.Blocks.Json.Tests.PrivatePropertyAndInheritance;
+
+//public class Person : PersonBase
+//{
+//    [JsonPropertyName("name")]
+//    public string Name { get; private set; } = "Mary";
+//    [Flatten]
+//    private Address Address { get; set; } = new();
+//    public Address GetAddress() => Address;
+//}
+
+
+//public class PersonBase : PersonCore
+//{
+//    public string BaseProperty { get; private set; } = "BaseProperty";
+//}
+
+//public class PersonCore
+//{
+
+//}
