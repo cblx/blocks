6c0ba8344753116b8c932bf1520388c0870003a2
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2026-01-19 11:46:37 -0300

    🩹 retira mecanismo de outputcache do rpcendpoint

--- RpcEndpoints/Server/MapEndpointExtensions.cs
+++ RpcEndpoints/Server/MapEndpointExtensions.cs
@@ -26,10 +26,18 @@ public static class MapEndpointExtensions
         if (endpoint.Cache != null)
         {
             // If response does not vary by custom logic, then we can use output caching
-            if (!endpoint.VariesServerCache)
-            {
-                routeBuilder.CacheOutput(pBuilder => pBuilder.AddPolicy<RpcCachePolicy>().Expire(endpoint.Cache.Value), excludeDefaultPolicy: true);
-            }
+            // Isso parece ter dado problema no dia 19/01/2026. (não é certeza, mas é suspeito)
+            // Basicamente passou a ocorrer erro 400 nos endpoints com cache habilitado, após uma publicação, e retornando corpo vazio.
+            // Acredito que foi feito esse outputcache pois em tese ele seria bem mais rápido de entregar
+            // do que usar o memorycache.
+            // Acabou ficando duas camadas de cache, "outputcache" e "filter+memorycache".
+            // Pensando melhor, o ganho maior do cache não é ter as coisas de forma bruta
+            // mas sim evitar acessar o banco de dados ou fazer cálculos pesados.
+            // Então, para simplificar a arquitetura, vou remover o outputcache.
+            //if (!endpoint.VariesServerCache)
+            //{
+            //    routeBuilder.CacheOutput(pBuilder => pBuilder.AddPolicy<RpcCachePolicy>().Expire(endpoint.Cache.Value), excludeDefaultPolicy: true);
+            //}
             routeBuilder.AddEndpointFilter(new RpcCacheFilter<TRequest>(endpoint));
         }
         if (endpoint.AllowAnonymous)
